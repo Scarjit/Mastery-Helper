735b850d5a19fc0176ce89927bb33e611e70adf2
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2022-05-25 18:38:42 +0200

    ci: added *.log to .gitignore

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"AramHelper/helper"
 	"AramHelper/lcu"
 	lol_champ_select "AramHelper/lcu/lol-champ-select/v1"
 	lol_login "AramHelper/lcu/lol-summoner/v1/current-summoner"
 	riot_api "AramHelper/riot-api"
-	"fmt"
-	"github.com/KnutZuidema/golio/riot/lol"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"log"
 	"os"
 	"os/exec"
 	"time"
@@ -16,12 +16,13 @@ import (
 var buildtime string
 
 func main() {
-	fmt.Printf("This is Mastery Helper v%s\n", buildtime)
+	helper.InitLogging(buildtime)
+	log.Printf("This is Mastery Helper v%s\n", buildtime)
 
 	// Get the API key from the environment
 	file, err := os.ReadFile("api_key.txt")
 	if err != nil {
-		fmt.Printf("Failed to read api_key.txt : %s\n", err)
+		log.Printf("Failed to read api_key.txt : %s\n", err)
 		time.Sleep(time.Second * 5)
 		return
 	}
@@ -31,7 +32,7 @@ func main() {
 	riotClient := riot_api.GetLolAPIClient()
 	_, err = riotClient.Riot.LoL.Summoner.GetByName("Scarjit")
 	if err != nil {
-		fmt.Printf("Failed to retrieve scarjitSummoner, your API_KEY might be invalid : %s\n", err)
+		log.Printf("Failed to retrieve scarjitSummoner, your API_KEY might be invalid : %s\n", err)
 		return
 	}
 
@@ -43,7 +44,7 @@ func main() {
 			time.Sleep(time.Second * 1)
 			lockfile = lcu.ReadLockFile()
 		}
-		fmt.Printf("League started\n")
+		log.Printf("League started\n")
 
 		// Get summoner
 		summoner := lol_login.GetSummoner(lockfile.Password, lockfile.Port)
@@ -51,74 +52,64 @@ func main() {
 			time.Sleep(time.Second * 1)
 			summoner = lol_login.GetSummoner(lockfile.Password, lockfile.Port)
 		}
-		fmt.Printf("Summoner logged in\n")
+		log.Printf("Summoner logged in\n")
 
 		// Wait for champ select
 		session := lol_champ_select.GetSession(lockfile.Password, lockfile.Port)
 		for session == nil {
 			time.Sleep(time.Second * 1)
+			log.Printf("Waiting for champ select\n")
 			session = lol_champ_select.GetSession(lockfile.Password, lockfile.Port)
 		}
-		fmt.Printf("Champ select started\n")
+		log.Printf("Champ select started\n")
 
 		t := table.NewWriter()
 		t.SetOutputMirror(os.Stdout)
 		t.AppendHeader(table.Row{"Champion", "Level", "Points", "Chest Granted", "Tokens Earned"})
 
-		for session.Timer.AdjustedTimeLeftInPhase > 0 {
+		for session.Timer.AdjustedTimeLeftInPhase > 0 || session.Timer.IsInfinite {
 			session = lol_champ_select.GetSession(lockfile.Password, lockfile.Port)
 			if session == nil {
-				fmt.Printf("Game was dodged !\n")
+				log.Printf("Game was dodged !\n")
 				break
 			}
 
-			var masteryInfo []struct {
-				Name    string
-				Mastery *lol.ChampionMastery
-			}
-			for _, championID := range session.BenchChampionIDS {
-				championMastery, championName, err := riot_api.GetChampionMasteryById(summoner.DisplayName, championID)
-				if err != nil {
-					fmt.Printf("Error while getting champion mastery : %s\n", err.Error())
-					continue
+			var masteryInfo []*riot_api.Mastery
+			if session.BenchEnabled {
+				for _, championID := range session.BenchChampionIDS {
+					championMastery, err := riot_api.GetChampionMasteryById(summoner.DisplayName, championID)
+					if err != nil {
+						log.Printf("Error while getting champion mastery : %s\n", err.Error())
+						continue
+					}
+					masteryInfo = append(masteryInfo, championMastery)
 				}
-				masteryInfo = append(masteryInfo, struct {
-					Name    string
-					Mastery *lol.ChampionMastery
-				}{
-					Name:    championName,
-					Mastery: championMastery,
-				})
 			}
 
 			for _, team := range session.MyTeam {
 				if team.ChampionID == 0 {
 					continue
 				}
-				championMastery, championName, err := riot_api.GetChampionMasteryById(summoner.DisplayName, int(team.ChampionID))
+				championMastery, err := riot_api.GetChampionMasteryById(summoner.DisplayName, int(team.ChampionID))
 				if err != nil {
-					fmt.Printf("Error getting mastery for %s\n", championName)
+					log.Printf("Error getting mastery for %s\n", team.ChampionID)
 					continue
 				}
-				masteryInfo = append(masteryInfo, struct {
-					Name    string
-					Mastery *lol.ChampionMastery
-				}{
-					Name:    championName,
-					Mastery: championMastery,
-				})
+				masteryInfo = append(masteryInfo, championMastery)
 			}
 
 			t.ResetRows()
 			for _, s := range masteryInfo {
+				if s != nil {
 
-				t.AppendRow(table.Row{
-					s.Name,
-					s.Mastery.ChampionLevel,
-					s.Mastery.ChampionPoints,
-					s.Mastery.ChestGranted,
-					s.Mastery.TokensEarned,
-				})
+					t.AppendRow(table.Row{
+						s.Name,
+						s.Level,
+						s.Points,
+						s.ChestGranted,
+						s.TokensEarned,
+					})
+				}
 			}
 
 			ClearScreen()
